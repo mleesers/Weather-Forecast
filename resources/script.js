572b87e1176b0a82ca0d842821a97eb4066a08e8
mleesers <mduron432@gmail.com>
2021-07-06 14:26:05 -0500

    added additional html and added URLs for API calls in javascript

---
+++ resources/script.js
@@ -0,0 +1,11 @@
+city = ""
+lat = ""
+lon = ""
+requestUrlMain = `api.openweathermap.org/data/2.5/weather?q=${city}&appid=0a1489eac7ff07297a4595ebe061c3c8`
+requestUrlSecondary = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=alerts&appid=0a1489eac7ff07297a4595ebe061c3c8`
+
+
+
+
+
+
