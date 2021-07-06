a8956622afabcf29028e6a77cb38d413f35689ea
mleesers <mduron432@gmail.com>
2021-07-06 16:01:36 -0500

    fixed a little of the styling in css and set up first API and succsesful data appends to index

--- resources/script.js
+++ resources/script.js
@@ -1,11 +1,29 @@
 city = ""
-lat = ""
-lon = ""
-requestUrlMain = `api.openweathermap.org/data/2.5/weather?q=${city}&appid=0a1489eac7ff07297a4595ebe061c3c8`
-requestUrlSecondary = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=alerts&appid=0a1489eac7ff07297a4595ebe061c3c8`
+lati = ""
+long = ""
+requestUrlSecondary = `https://api.openweathermap.org/data/2.5/onecall?lat=${lati}&lon=${long}&exclude=alerts&appid=0a1489eac7ff07297a4595ebe061c3c8`
 
 
+function getApi(){
+    city = document.getElementById("city").value;
+    requestUrlMain = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=0a1489eac7ff07297a4595ebe061c3c8`
+    fetch(requestUrlMain)
+    .then(function (response){
+        return response.json();
+    })
+    .then(function (data) {
+        document.querySelector(".City").textContent = data.name;
+        console.log(data)
+        console.log(data.main.temp)
+        temperature = ((data.main.temp -273.15) * (9/5) + 32);
+        document.getElementById("temp").textContent = "Temp: " + temperature.toFixed(2) + "\u00B0F";
+        document.getElementById("wind").textContent = "Wind: " + data.wind.speed;
+        document.getElementById("humidity").textContent = "Humidity: " + data.main.humidity;
+        lati = data.coord.lat;
+        long = data.coord.lon;
+        console.log(data)
+    });
+    return lati,long;
+}
 
-
-
-
+document.querySelector(".search").addEventListener("click", getApi);
