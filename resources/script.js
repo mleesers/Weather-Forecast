9bc04d8cb354d61db55ef18f44610afab49cc91c
mleesers <mduron432@gmail.com>
2021-07-06 23:53:40 -0500

    generate buttons based on searches, couldnt figure out button clicks

--- resources/script.js
+++ resources/script.js
@@ -7,10 +7,58 @@ for(i=1;i<=5;i++){
 }
 function localStore(){
     localStorage.setItem("cityArray",JSON.stringify(cityArray));
-    storedCities = JSON.parse(localStorage.getItem("cityArray"))
+    storedCities = localStorage.getItem("cityArray");
+    arr = [];
+    for(i=0;i<storedCities.length;i++){
+        if(!arr.includes(city)){
+            btn = document.createElement("button");
+            btn.innerHTML = city;
+            btn.setAttribute('class','result');
+            btn.setAttribute('id', city);
+            document.querySelector(".results").appendChild(btn);
+            arr.push(city);
+        }else{
+            continue;
+        }
+    }
+    createEventListener(arr);
 }
 
+function createEventListener(){
+    lastItem = arr[arr.length-1]
+    ind = arr.indexOf(lastItem)
+    for(i=0;i<arr.length;i++){
+        butt = document.getElementById(arr[ind]).textContent;
+        document.getElementById(butt).addEventListener("click",function(event){
+            buttonApi(butt);
+            event.preventDefault();
+        });
+    }
+}
+    
 
+function buttonApi(){
+    city = document.getElementsById(butt).textContent;
+    console.log(city);
+    requestUrlMain = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=0a1489eac7ff07297a4595ebe061c3c8`
+    fetch(requestUrlMain)
+    .then(function (response){
+        return response.json();
+    })
+    .then(function (data) {
+        document.querySelector(".City").textContent = data.name;
+        temperature = ((data.main.temp -273.15) * (9/5) + 32);
+        document.getElementById("temp").textContent = "Temp: " + temperature.toFixed(2) + "\u00B0F";
+        document.getElementById("wind").textContent = "Wind: " + data.wind.speed + "mph";
+        document.getElementById("humidity").textContent = "Humidity: " + data.main.humidity + "%";
+        lati = data.coord.lat;
+        long = data.coord.lon;
+        requestUrlSecondary = `https://api.openweathermap.org/data/2.5/onecall?lat=${lati}&lon=${long}&exclude=alerts,minutely,hourly&appid=0a1489eac7ff07297a4595ebe061c3c8`
+        getSecondApi();
+        return requestUrlSecondary;
+    });
+    
+}
 
 
 function getApi(){
@@ -52,11 +100,12 @@ function getSecondApi(){
 document.querySelector(".search").addEventListener("click", function(){
     getApi();
     city = document.getElementById("city").value;
-    console.log(city);
     if(!cityArray.includes(city)){
-        cityArray.push(city);   
+        cityArray.push(city);
+
     }else{
         window.alert("That city has already been entered");
     }
-    localStore();
+    localStore(city);
+    
 });
